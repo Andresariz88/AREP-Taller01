f1483e54f6eef8ace7527ee323d5d19db0464c7d
Andrés Ariza <andres-ariza70@hotmail.com>
2023-01-31 22:05:29 -0500

    Http server

--- src/main/java/org/arep/HttpServer.java
+++ src/main/java/org/arep/HttpServer.java
@@ -6,12 +6,13 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.*;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class HttpServer {
 
     private static final String USER_AGENT = "Mozilla/5.0";
-    private static final String GET_URL = "https://omdbapi.com/?s=%S&apikey=1d53bda9";
-    private static HashMap<String, String> cache = new HashMap<>();
+    private static final String GET_URL = "https://omdbapi.com/?t=%S&apikey=1d53bda9";
+    private static final ConcurrentHashMap<String, String> cache = new ConcurrentHashMap<>();
 
     public static void main(String[] args) throws IOException {
         ServerSocket serverSocket = null;
@@ -56,20 +57,11 @@ public class HttpServer {
             String requestedMovie = null;
             //System.out.println("FILE, METHOD: " + request + " " + method);
             if (request.startsWith("/form?") && method.equals("POST")) {
-                requestedMovie = request.split("s=")[1];
+                requestedMovie = request.replace("/form?s=", "");
                 outputLine = "HTTP/1.1 200 OK\r\n" +
                         "Content-type: application/json\r\n"+
-                        "\r\n"/* +
-                        "<!DOCTYPE html>"
-                        + "<html>"
-                        + "<head>"
-                        + "<meta charset=\"UTF-8\">"
-                        + "<title>Title of the document</title>\n"
-                        + "</head>"
-                        + "<body>"*/
-                        + getMovie(requestedMovie)
-                        /*+ "</body>"
-                        + "</html>"*/;
+                        "\r\n"
+                        + getMovie(requestedMovie.toLowerCase());
             } else {
                 outputLine = htmlGetForm();
             }
@@ -91,7 +83,7 @@ public class HttpServer {
         String reqMovie = "";
         if (cache.containsKey(movie)) {
             reqMovie = cache.get(movie);
-            System.out.println("Película " + movie + " recuperada.");
+            //System.out.println("Película " + movie + " recuperada.");
             return reqMovie;
         }
         String formatted = String.format(GET_URL, movie);
@@ -116,7 +108,7 @@ public class HttpServer {
             in.close();
             reqMovie = response.toString();
             cache.put(movie, reqMovie);
-            System.out.println("Película " + movie + " guardada.");
+            //System.out.println("Película " + movie + " guardada.");
         } else {
             System.out.println("GET request not worked");
         }
@@ -138,26 +130,90 @@ public class HttpServer {
                 "<!DOCTYPE html>\n" +
                 "<html>\n" +
                 "  <head>\n" +
-                "    <title>Form Example</title>\n" +
+                "    <title>Búsqueda</title>\n" +
                 "    <meta charset=\"UTF-8\">\n" +
                 "    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+                "    <style>\n" +
+                "      @import url(\"https://fonts.googleapis.com/css?family=Roboto&display=swap\");\n" +
+                "\n" +
+                "      * {\n" +
+                "        font-family: \"Roboto\", sans-serif;\n" +
+                "        background-color: #f5f6fa;\n" +
+                "      }\n" +
+                "\n" +
+                "      h1 {\n" +
+                "        padding: 5px;\n" +
+                "        margin: 15px 0px;\n" +
+                "      }\n" +
+                "\n" +
+                "      .form {\n" +
+                "        padding: 5px;\n" +
+                "      }\n" +
+                "\n" +
+                "      .form label {\n" +
+                "        margin: 5px 2px;\n" +
+                "      }\n" +
+                "\n" +
+                "      .form input {\n" +
+                "        margin: 5px 2px;\n" +
+                "        padding: 8px;\n" +
+                "        font-size: 15px;\n" +
+                "        border-radius: 3px;\n" +
+                "        border: 1px solid rgba(0, 0, 0, 0);\n" +
+                "        box-shadow: 0 6px 10px 0 rgba(0, 0, 0 , .15);\n" +
+                "        transition: all 200ms ease;\n" +
+                "      }\n" +
+                "\n" +
+                "      .form input:hover {\n" +
+                "        border: 1px solid rgba(0, 0, 0, 0.281);\n" +
+                "        box-shadow: 0 6px 10px 0 rgba(0, 0, 0 , .22);\n" +
+                "      }\n" +
+                "\n" +
+                "      .form input:focus {\n" +
+                "        outline: none !important;\n" +
+                "        border: 1px solid #6c7ff2;\n" +
+                "      }\n" +
+                "\n" +
+                "      .btn {\n" +
+                "        color: white;\n" +
+                "        background-color: #7f8ff4;\n" +
+                "        transition: all 200ms ease;\n" +
+                "        cursor: pointer;\n" +
+                "      }\n" +
+                "\n" +
+                "      .btn:hover {\n" +
+                "        background-color: #6c7ff2;\n" +
+                "      }\n" +
+                "\n" +
+                "      .container {\n" +
+                "        margin: 5px 2px;\n" +
+                "        padding: 8px;\n" +
+                "      }\n" +
+                "\n" +
+                "    </style>\n" +
                 "  </head>\n" +
                 "\n" +
                 "  <body>\n" +
-                "    <h1>Form with POST</h1>\n" +
-                "    <form action=\"/hellopost\">\n" +
-                "      <label for=\"postname\">Name:</label><br>\n" +
-                "      <input type=\"text\" id=\"postname\" name=\"name\" value=\"John\" required><br><br>\n" +
-                "      <input type=\"button\" value=\"Submit\" onclick=\"loadPostMsg(postname)\">\n" +
+                "    <h1>Busca una Película</h1>\n" +
+                "    <form class=\"form\" action=\"/form\">\n" +
+                "      <label for=\"postname\">Nombre:</label><br>\n" +
+                "      <input type=\"text\" id=\"postname\" name=\"s\" value=\"John\" placeholder=\"Ingresa el nombre\" required><br>\n" +
+                "      <input class=\"btn\" type=\"button\" value=\"Buscar\" onclick=\"loadPostMsg(postname)\">\n" +
                 "    </form>\n" +
-                "      <div id=\"postrespmsg\"></div>\n" +
+                "      <div class=\"container\" id=\"postrespmsg\"></div>\n" +
                 "      <script>\n" +
                 "        function loadPostMsg(name){\n" +
-                "            let url = \"/form?s=\" + name.value;" +
+                "          let movie;\n" +
+                "            let url = \"/form?s=\" + name.value;\n" +
                 "\n" +
                 "            fetch (url, {method: 'POST'})\n" +
-                "                .then(x => x.text())\n" +
-                "                .then(y => document.getElementById(\"postrespmsg\").innerHTML = y);\n" +
+                "                .then(response => response.json())\n" +
+                "                .then(data => {\n" +
+                "                  let div = document.getElementById(\"postrespmsg\");\n" +
+                "\n" +
+                "                  document.getElementById(\"postrespmsg\").innerHTML = data;\n" +
+                "                } /*document.getElementById(\"postrespmsg\").innerHTML = data*/);\n" +
+                "            \n" +
                 "          }\n" +
                 "      </script>\n" +
                 "  </body>\n" +
--- src/test/java/HttpConnectionTest.java
+++ src/test/java/HttpConnectionTest.java
@@ -1,4 +1,3 @@
-import org.arep.HttpConnection;
 import org.arep.HttpServer;
 import org.junit.*;
 
@@ -13,8 +12,8 @@ public class HttpConnectionTest {
     @Test
     public void deberiaRetornarPelicula() {
         try {
-            String busqueda = HttpServer.getMovie("Guardians of the Galaxy Vol. 2");
-            String guardians = "{\"Search\":[{\"Title\":\"Guardians of the Galaxy Vol. 2\",\"Year\":\"2017\",\"imdbID\":\"tt3896198\",\"Type\":\"movie\",\"Poster\":\"https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg\"},{\"Title\":\"Bonus Round: The Making of 'Guardians of the Galaxy Vol. 2'\",\"Year\":\"2017\",\"imdbID\":\"tt7312152\",\"Type\":\"movie\",\"Poster\":\"N/A\"},{\"Title\":\"Guardians of the Galaxy Vol. 2 (2017)\",\"Year\":\"2017\",\"imdbID\":\"tt17977702\",\"Type\":\"movie\",\"Poster\":\"N/A\"},{\"Title\":\"Chris Pratt Shows You Around the Set of Guardians of the Galaxy Vol. 2\",\"Year\":\"2016\",\"imdbID\":\"tt5630492\",\"Type\":\"movie\",\"Poster\":\"N/A\"}],\"totalResults\":\"4\",\"Response\":\"True\"}";
+            String busqueda = HttpServer.getMovie("Guardians of the Galaxy Vol 2");
+            String guardians = "{\"Title\":\"Guardians of the Galaxy Vol. 2\",\"Year\":\"2017\",\"Rated\":\"PG-13\",\"Released\":\"05 May 2017\",\"Runtime\":\"136 min\",\"Genre\":\"Action, Adventure, Comedy\",\"Director\":\"James Gunn\",\"Writer\":\"James Gunn, Dan Abnett, Andy Lanning\",\"Actors\":\"Chris Pratt, Zoe Saldana, Dave Bautista\",\"Plot\":\"The Guardians struggle to keep together as a team while dealing with their personal family issues, notably Star-Lord's encounter with his father the ambitious celestial being Ego.\",\"Language\":\"English\",\"Country\":\"United States\",\"Awards\":\"Nominated for 1 Oscar. 15 wins & 60 nominations total\",\"Poster\":\"https://m.media-amazon.com/images/M/MV5BNjM0NTc0NzItM2FlYS00YzEwLWE0YmUtNTA2ZWIzODc2OTgxXkEyXkFqcGdeQXVyNTgwNzIyNzg@._V1_SX300.jpg\",\"Ratings\":[{\"Source\":\"Internet Movie Database\",\"Value\":\"7.6/10\"},{\"Source\":\"Rotten Tomatoes\",\"Value\":\"85%\"},{\"Source\":\"Metacritic\",\"Value\":\"67/100\"}],\"Metascore\":\"67\",\"imdbRating\":\"7.6\",\"imdbVotes\":\"687,044\",\"imdbID\":\"tt3896198\",\"Type\":\"movie\",\"DVD\":\"22 Aug 2017\",\"BoxOffice\":\"$389,813,101\",\"Production\":\"N/A\",\"Website\":\"N/A\",\"Response\":\"True\"}";
             Assert.assertEquals(guardians, busqueda);
         } catch (IOException e) {
             throw new RuntimeException(e);
